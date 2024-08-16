32a0b7b16c5546b9a398469af85a6f729b4f2125
Sarajit-mondal <bdsarajit499@gmail.com>
2024-08-16 12:38:01 +0600

    nav bar design complete

---
+++ src/component/products/ProductCard.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable react/jsx-key */
+const ProductCard = () => {
+  const product = {
+    thumbnail:
+      "https://cdn.dummyjson.com/products/images/beauty/Powder%20Canister/1.png",
+    title: "Sample Product Title",
+    price: 49.99,
+    discountPercentage: 20,
+    rating: 4.5,
+  };
+  // Calculate discounted price
+  const discountedPrice = (
+    product.price -
+    (product.price / 100) * product.discountPercentage
+  ).toFixed(2);
+
+  // Generate random review count between 10 and 210
+  const totalReviews = Math.ceil(Math.random() * 200 + 10);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-5">
+      {[...Array(10)].map(() => (
+        <div className="border rounded-lg p-4 shadow-lg hover:shadow-xl transition-shadow duration-300">
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="w-11/12 h-52 md:h-40 object-cover rounded-md mb-4 mx-auto"
+          />
+          <h2 className="text-lg font-semibold mb-2">
+            {product.title.length > 20
+              ? product.title.slice(0, 20) + "..."
+              : product.title}
+          </h2>
+          <div className="product-price text-sm text-gray-700 mb-4 flex justify-between">
+            <h4>
+              Price:{" "}
+              <span className="font-bold text-green-500">${product.price}</span>
+            </h4>
+            <h4>
+              Discount:{" "}
+              <span className="font-bold text-red-500">
+                {product.discountPercentage}%
+              </span>
+            </h4>
+          </div>
+          <div className="ratting-stock text-sm text-gray-700 mb-4 flex justify-between">
+            <h4>
+              Rating: <span className="font-bold">{product.rating}</span>
+            </h4>
+            <h4>
+              Total Reviews: <span className="font-bold">{totalReviews}</span>
+            </h4>
+          </div>
+          <div className="text-center">
+            <button className="add w-full active:scale-[98%] ease-linear duration-150 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
+              + ${discountedPrice}
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import FilterControls from '../../component/products/FilterControls'
+import FilterControls from "../../component/products/FilterControls";
+import ProductCard from "../../component/products/ProductCard";
 
 function Home() {
   return (
     <div>
-      
       <div>
-         <FilterControls />
+        <h2 className="text-4xl font-bold text-center mt-8">All Products</h2>
+        <FilterControls />
+      </div>
+      <div>
+        <ProductCard />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
